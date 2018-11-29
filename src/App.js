fd79e01b7d1bb909cd9f487dae648562cbf963bf
William Cao <williamcao.cmu@gmail.com>
2018-11-29 23:49:25 +0700

    set up route and create ui dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import Login from './components/login/Login';
 import Register from './components/register/Register';
+import Dashboard from './components/Dashboard';
+
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Register />
+        <BrowserRouter>
+        <Switch>
+          <Route path='/register' exact component={Register} ></Route>
+          <Route path='/login' exact component={Login} ></Route>
+          <Route path='/dashboard' exact component={Dashboard} ></Route>
+        </Switch>
+        </BrowserRouter>
       </div>
     );
   }
